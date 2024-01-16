7e3085c4df0db26205c3a24f3e4992d8d0e6d2ae
JulKuzmi <enchantress000@mail.ru>
2024-01-16 20:14:29 +0300

    layout

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { MainPage } from "./pages/MainPage/MainPage";
-
+import { PageLayout } from "./pages/LayoutPage/LayoutPage";
 import { NotFound } from "./pages/NotFoundPage/NotFound";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route index element={<MainPage />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<PageLayout />}>
+        <Route index element={<MainPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   );
 };
